3015e32fec16fdce2dd9d6762010a767e8f809d5
Matthias Lodner <matthias.lodner@uni-ulm.de>
2020-07-13 01:05:49 +0200

    Removed test lines.

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,16 @@ fn main() {
 
     let registry: Registry = Registry::new("to_scrape_shopware.json".to_string());
 
+    println!("{} plugins to check:", registry.plugins.len());
     for plugin in &registry.plugins {
         plugin.print_information();
     }
 
-    println!("Plugins to update:");
     let updatables: Vec<Updatable> = registry.check_for_updates();
+    println!("\n{} plugins to update:", updatables.len());
     for updatable in updatables {
         println!("    - {}", updatable);
     }
-
-    let url = "https://store.shopware.com/media57848636557/facebook-pixel-einbinden.html";
-
-    let version_string = String::from("1.6.6");
-    let plugin: Plugin = Plugin::new("Test Plugin".to_string(), version_string, url.to_string());
-    plugin.print_information();
-
-    scraping::shopware::scrape_plugin(&plugin);
 
     println!("End main");
 }
